dcbe4b6d3ebbbc353ec8d68422cd4e5df5fd4429
kevinelong <kevinelong@gmail.com>
2023-10-20 11:48:23 -0700

    comment test code for nodejs

--- scripts/payment_calculator.js
+++ scripts/payment_calculator.js
@@ -18,10 +18,10 @@ function calculate(term, apr, price){
 }
 
 //TESTING
-let term = 25; // 30 years
-let apr = 3.3; //Annual percentage rate
-const price = 350000; //price of house
+// let term = 25; // 30 years
+// let apr = 3.3; //Annual percentage rate
+// const price = 350000; //price of house
 
-const payment = calculate(term, apr, price)
-console.log(payment); //expected 1714.8654830497117
+// const payment = calculate(term, apr, price)
+// console.log(payment); //expected 1714.8654830497117
 
